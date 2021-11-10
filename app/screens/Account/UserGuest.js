6d3cc87ef7be48c822bd22d7be538b8f24aa6b8c
Alexander Urrutia <aurrutia.externo@coopeuch.cl>
2021-11-10 15:25:22 -0300

    Create component Loading

--- app/screens/Account/UserGuest.js
+++ app/screens/Account/UserGuest.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { View, Text } from "react-native";
 
-const Account = () => {
+const UserGuest = () => {
   return (
     <View>
-      <Text>Account</Text>
+      <Text>UserGuest</Text>
     </View>
   );
 };
 
-export default Account;
+export default UserGuest;
