061f7d53dfe99abbf4a672c5fedd9143fade141f
Alexander Urrutia <aurrutia.externo@coopeuch.cl>
2021-11-11 11:55:33 -0300

    Validate form register

--- app/components/Account/RegisterForm.js
+++ app/components/Account/RegisterForm.js
@@ -1,15 +1,42 @@
 import React, { useState } from "react";
 import { StyleSheet, View } from "react-native";
 import { Input, Icon, Button } from "react-native-elements";
+import { validateEmail } from "../../utils/validations";
+import { size, isEmpty } from "lodash";
 
 const RegisterForm = () => {
   const [securityPassword, setSecurityPassword] = useState(true);
   const [securityPasswordRepeat, setSecurityPasswordRepeat] = useState(true);
+  const [formData, setFormData] = useState(defaultFormValue);
+
+  const onSubmit = () => {
+    if (
+      isEmpty(formData.email) ||
+      isEmpty(formData.password) ||
+      isEmpty(formData.repeatPassword)
+    ) {
+      console.log("todo los campos son obligatorios");
+    } else if (!validateEmail(formData.email)) {
+      console.log("email no valido");
+    } else if (formData.password !== formData.repeatPassword) {
+      console.log("contraseñas no son iguales");
+    } else if (size(formData.password) < 6) {
+      console.log("la contraseña tiene q tener al menos 6 caracteres");
+    } else {
+      console.log("ok");
+    }
+  };
+
+  const onChange = (e, type) => {
+    const { text } = e.nativeEvent;
+    setFormData({ ...formData, [type]: text });
+  };
   return (
     <View style={styles.formContainer}>
       <Input
         placeholder="Correo electrónico"
         containerStyle={styles.inputForm}
+        onChange={(e) => onChange(e, "email")}
         rightIcon={
           <Icon type="material-community" name="at" iconStyle={styles.icon} />
         }
@@ -17,6 +44,7 @@ const RegisterForm = () => {
       <Input
         placeholder="Contraseña"
         containerStyle={styles.inputForm}
+        onChange={(e) => onChange(e, "password")}
         password={securityPassword}
         secureTextEntry={securityPassword}
         rightIcon={
@@ -31,6 +59,7 @@ const RegisterForm = () => {
       <Input
         placeholder="Repetir contraseña"
         containerStyle={styles.inputForm}
+        onChange={(e) => onChange(e, "repeatPassword")}
         password={securityPasswordRepeat}
         secureTextEntry={securityPasswordRepeat}
         rightIcon={
@@ -46,12 +75,21 @@ const RegisterForm = () => {
         title="Unirse"
         containerStyle={styles.btnContainerRegister}
         buttonStyle={styles.btnRegister}
+        onPress={onSubmit}
       />
     </View>
   );
 };
 
 export default RegisterForm;
+
+function defaultFormValue() {
+  return {
+    email: "",
+    password: "",
+    repeatPassword: "",
+  };
+}
 
 const styles = StyleSheet.create({
   formContainer: {
